5eba6c6182f8475b8db1044765adbbfda8215e44
ioSquishy <squishylxix@gmail.com>
2026-02-23 14:38:30 -0800

    made ItineraryDays collapsable

--- togo/components/ItineraryDay.tsx
+++ togo/components/ItineraryDay.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { useEffect, type ReactNode } from 'react';
 import ItemContainer from "./ItemContainer";
 
 interface ItineraryDayProps {
@@ -13,13 +13,58 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 export default function ItineraryDay({date, children} : ItineraryDayProps) {
+  const dateStr = date.toLocaleDateString();
+  const dateStrWithHyphens = dateStr.replaceAll("/", "-");
+  const buttonId = `collapseButton-${dateStrWithHyphens}`;
+  const imgId = `collapseImg-${dateStrWithHyphens}`;
+  const divId = `collapseDiv-${dateStrWithHyphens}`;
+
+  useEffect(() => {
+    const collapseImg = document.querySelector(`#${imgId}`) as HTMLImageElement;
+    const collapseDiv = document.querySelector(`#${divId}`) as HTMLDivElement;
+    const collapseButton = document.querySelector(`#${buttonId}`) as HTMLButtonElement;
+
+    if (collapseButton == null) {
+      console.log("Failed to query select collapseButton.");
+      return;
+    } else {
+      collapseButton.addEventListener("click", collapseContainer);
+    }
+    
+    let isCollapsed = false;
+    function collapseContainer() {
+      if (collapseImg === null) {
+        return;
+      }
+      if (isCollapsed) {
+        // uncollapse
+        collapseImg.src = "/collapse_icon-open.svg";
+        collapseDiv.classList.remove("hidden");
+      } else {
+        // collapse
+        collapseImg.src = "/collapse_icon-closed.svg";
+        collapseDiv.classList.add("hidden");
+      }
+      isCollapsed = !isCollapsed;
+    }
+
+    return () => {
+      collapseButton.removeEventListener("click", collapseContainer);
+    };
+  }, []);
+
   return (
     <div>
-      <hr></hr>
-      <h3>{dateFormatter.format(date)}</h3>
-      <ItemContainer id={date.toLocaleDateString()} wishlist={false}>
-        {children}
-      </ItemContainer>
+      <hr className="mb-3"></hr>
+      <div className="flex mb-1">
+        <button id={buttonId} className="w-10"><img id={imgId} src="/collapse_icon-open.svg" /></button>
+        <h3 className="m-0">{dateFormatter.format(date)}</h3>
+      </div>
+      <div id={divId}>
+        <ItemContainer id={dateStrWithHyphens} wishlist={false}>
+          {children}
+        </ItemContainer>
+      </div>
     </div>
   );
 }
