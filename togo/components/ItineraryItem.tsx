24a68d37ef8b7adaa75f102331b842dbcb3d3503
ioSquishy <squishylxix@gmail.com>
2026-02-28 19:45:11 -0800

    added cancel saving note button to ItineraryItem

--- togo/components/ItineraryItem.tsx
+++ togo/components/ItineraryItem.tsx
@@ -1,5 +1,5 @@
 import { Draggable } from "@hello-pangea/dnd";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export interface ItineraryItemProps {
   id: number;
@@ -27,12 +27,22 @@ export default function ItineraryItem({ id, index, wishlistItem, destName, destD
   }
 
   const [isEditingNote, setIsEditingNote] = useState(false);
+  const [originalNote, setOriginalNote] = useState(itemNote || "");
+  const noteRef = useRef<HTMLTextAreaElement>(null);
   function editNote() {
-    setIsEditingNote(prev => true);
+    setIsEditingNote(true);
   }
   function saveNote() {
-    setIsEditingNote(prev => false);
+    setOriginalNote(noteRef.current?.value || "");
+    setIsEditingNote(false);
     // TODO: update database
+  }
+  function cancelNoteChanges() {
+    if (noteRef.current) {
+      noteRef.current.value = originalNote;
+    }
+    setIsEditingNote(false);
+    
   }
 
   return (
@@ -62,10 +72,16 @@ export default function ItineraryItem({ id, index, wishlistItem, destName, destD
               <div className="border-t-1 border-dashed overflow-hidden mx-2" hidden={isExpanded ? false : wishlistItem || !itemNote}>
                 <div className="flex gap-2 my-1">
                   <img className="self-start py-2" src="/note_icon.svg" alt="Note icon"></img>
-                  <textarea readOnly={!isEditingNote} onClick={editNote} className={`self-center bg-gray-100 px-3 py-1 rounded-lg field-sizing-content resize-none text-md/5 ${isEditingNote ? "w-full" : "truncate"}`} defaultValue={itemNote} />
+                  <textarea ref={noteRef} readOnly={!isEditingNote} onClick={editNote} className={`self-center bg-gray-100 px-3 py-1 rounded-lg field-sizing-content resize-none text-md/5 ${isEditingNote ? "w-full" : "truncate"}`} defaultValue={originalNote} />
                   <button hidden={!isEditingNote} onClick={saveNote} className="save-note-button self-end p-2 bg-blue-100 rounded-lg">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" clipRule="evenodd" fill="gray" d="M18.1716 1C18.702 1 19.2107 1.21071 19.5858 1.58579L22.4142 4.41421C22.7893 4.78929 23 5.29799 23 5.82843V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H18.1716ZM4 3C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21L5 21L5 15C5 13.3431 6.34315 12 8 12L16 12C17.6569 12 19 13.3431 19 15V21H20C20.5523 21 21 20.5523 21 20V6.82843C21 6.29799 20.7893 5.78929 20.4142 5.41421L18.5858 3.58579C18.2107 3.21071 17.702 3 17.1716 3H17V5C17 6.65685 15.6569 8 14 8H10C8.34315 8 7 6.65685 7 5V3H4ZM17 21V15C17 14.4477 16.5523 14 16 14L8 14C7.44772 14 7 14.4477 7 15L7 21L17 21ZM9 3H15V5C15 5.55228 14.5523 6 14 6H10C9.44772 6 9 5.55228 9 5V3Z"/>
+                    </svg>
+                  </button>
+                  <button hidden={!isEditingNote} onClick={cancelNoteChanges} className="cancel-note-button self-end p-2 bg-gray-100 rounded-lg">
+                    <svg width="16" height="16" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" strokeWidth="3" stroke="#7d3d3d" fill="none">
+                      <line x1="8.06" y1="8.06" x2="55.41" y2="55.94" />
+                      <line x1="55.94" y1="8.06" x2="8.59" y2="55.94" />
                     </svg>
                   </button>
                 </div>
